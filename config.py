b4437aa8a86c9bfcd42f869f7f21f5fefc3056a0
nkwbtb <nkwbtb@outlook.com>
2023-06-06 02:47:14 -0500

    add LoRA

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import torch
 
 MODEL_NAME = 'henry931007/mfma'
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-BATCH_SIZE = 32
+BATCH_SIZE = 5
 EPOCH = 10
-LR = 2e-5
+LR = 1e-3
 EXP_PATH = "exp/"
--- model.py
+++ model.py
@@ -12,12 +12,14 @@ from sklearn.metrics import balanced_accuracy_score, f1_score
 from scipy.special import expit
 from datasets import Dataset
 from summac.benchmark import SummaCBenchmark
+from peft import get_peft_config, PeftModel, PeftConfig, get_peft_model, LoraConfig, TaskType
 import logging
 
 MODEL_NAME = 'microsoft/deberta-large-mnli'
 DATA_TRAIN = 'factcc'
 REMOVE_COLUMNS = ['filepath', 'id', 'annotations', 'dataset', 'origin']
-FREEZE = True
+FREEZE = False
+LORA = True
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
@@ -39,6 +41,17 @@ def compute_metrics(eval_pred):
                "f1": f1_score(labels, predictions, average="macro")}
     return results
 
+class CustomTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get('logits')
+        # compute custom loss
+        loss_fct = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor([15/85]).to(CFG.DEVICE))
+        loss = loss_fct(logits, labels)
+        return (loss, outputs) if return_outputs else loss
+
 if __name__ == '__main__':
     benchmark_val = SummaCBenchmark(benchmark_folder="./summac_benchmark/", cut="val")
     benchmark_test = SummaCBenchmark(benchmark_folder="./summac_benchmark/", cut="test")
@@ -56,27 +69,54 @@ if __name__ == '__main__':
                                                                 num_labels=1, 
                                                                 problem_type="multi_label_classification",
                                                                 ignore_mismatched_sizes=True)
-    
-    if FREEZE:
-        for name, param in model.named_parameters():
-            if not name.startswith("deberta.encoder.layer.23.attention.self"):
-                # and not name.startswith("encoder.layer.11.intermediate"):
+
+    if LORA:
+        peft_config = LoraConfig(
+            task_type = TaskType.SEQ_CLS,
+            r = 8,
+            lora_alpha = 8,
+            lora_dropout = 0.1,
+            inference_mode = False, 
+            target_modules='.*attention\.self.*proj',
+            bias='none'
+        )
+        model = get_peft_model(model, peft_config)
+        model.print_trainable_parameters()
+        import pdb
+        pdb.set_trace()
+
+    trainable_params = 0
+    all_param = 0
+    for name, param in model.named_parameters():
+        if FREEZE:
+            if not name.startswith("deberta.encoder.layer.23.attention.self") \
+                and not name.startswith("classifier"):
                 param.requires_grad = False
-            print(name, param.requires_grad)
+        print(name, param.requires_grad)
+        num_params = param.numel()
+            # if using DS Zero 3 and the weights are initialized empty
+        if num_params == 0 and hasattr(param, "ds_numel"):
+            num_params = param.ds_numel
+
+        all_param += num_params
+        if param.requires_grad:
+            trainable_params += num_params
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
 
     model.to(CFG.DEVICE)
-    optimizer = torch.optim.Adam(model.parameters(), lr = CFG.LR)
     training_args = TrainingArguments(output_dir="test_trainer", 
                                       evaluation_strategy="epoch",
                                       num_train_epochs=CFG.EPOCH,
+                                      learning_rate=CFG.LR,
                                       per_device_train_batch_size=CFG.BATCH_SIZE)
-    trainer = Trainer(
+    trainer = CustomTrainer(
         model=model,
         args=training_args,
         train_dataset=train_set,
         eval_dataset=test_set,
         compute_metrics=compute_metrics,
-        optimizers = (optimizer, None)
     )
 
     trainer.train()
